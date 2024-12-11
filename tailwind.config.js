5cb24c3dc8d9ad2685e2b09c3b57a49270e643d4
Mohammed Arshad <arshadcheruputhore@gmail.com>
2024-12-11 13:07:02 +0530

    src deleted,root folder altred

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html", "./src/**/*.js"],
+  content: [
+    './index.html',  // Include the HTML file in the root
+    './main.js',     // Include the JavaScript file in the root
+  ],
   theme: {
     extend: {},
   },
